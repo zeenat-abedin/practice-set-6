d0cb62b93a48ebe057f14a4a8a757e0379c4f908
zeenat <zeenu.23@gmail.com>
2024-03-18 21:16:23 +0530

    Update the RailwayForm class to accept an additional parameter for train number.

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ class RailwayForm(){
     cancel(){
         alert(this.name + "The form is cancelled")
     }
-    fill(givenname){
+    fill(givenname, trainno){
         this.name = givenname;
+        this.trainNumber=trainno;
     }
 }
 
 let harryForm = new RailwayForm()
-harryForm.fill('Harry')
+harryForm.fill('Harry', 123456)
+
 let rohanForm = new RailwayForm()
-rohanForm.fill('Rohan')
+rohanForm.fill('Rohan', 4567890)
 
 harryForm.submit(); // Output: The form is submitted.
 rohanForm.submit(); 
