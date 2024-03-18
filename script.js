f8e44de4d1775b96f7344e4bb28a8678328ed50f
zeenat <zeenu.23@gmail.com>
2024-03-18 23:10:31 +0530

    Add time and address fields to the RailwayForm class.

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 class RailwayForm{
-    constructor(givenname, trainno, address){
+    constructor(givenname, trainno, address, time){
         console.log("Constructor is called" + " " + givenname + " " + trainno)
         this.givenname = givenname;
         this.trainno = trainno;
-        this.address= address;
+        this.address = address;
+        this.time=time;
     }
     submit(){
-        alert(this.name + " " + "The form is submitted")
+        alert(this.name + " " + "The form is submitted for train number" + " " + this.trainno);
     }
     cancel(){
-        alert(this.name + " " + "The form is cancelled")
+        alert(this.name + " " + "The form is cancelled for train number" + " " + this.trainno)
+        this.trainno = 0
     }
     fill(givenname, trainno){
         this.name = givenname;
-        this.trainNumber=trainno;
+        this.trainNumber = trainno;
+        this.address = address;
+        this.time = time
     }
 }
 
-let harryForm = new RailwayForm()
-harryForm.fill('Harry', 123456)
+// let harryForm = new RailwayForm()
+// harryForm.fill('Harry', 123456)
 
-let rohanForm = new RailwayForm()
-rohanForm.fill('Rohan', 4567890)
+// let rohanForm = new RailwayForm()
+// rohanForm.fill('Rohan', 4567890)
 
-harryForm.submit(); // Output: The form is submitted.
-rohanForm.submit(); 
-rohanForm.cancel(); // Output: The form is cancelled.
+// harryForm.submit(); 
+// rohanForm.submit(); 
+// rohanForm.cancel(); 
