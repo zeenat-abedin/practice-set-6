7dc592ae7986faa158bba6913da8ab513ecc10cf
zeenat <zeenu.23@gmail.com>
2024-03-23 17:15:24 +0530

    "Added display of todo item and cleared input fields after submission; cleared todo item on delete"

--- script2.js
+++ script2.js
@@ -2,10 +2,15 @@ submit.addEventListener('click',(e) => {
     e.preventDefault()
     let titleContent = title.value
     let descContent = desc.value
-    localStorage.setItem('todo', JSON.stringify([titleContent,descContent]))
+    localStorage.setItem('todo', JSON.stringify([titleContent, descContent]))
+    todo.innerHTML = `<h1>${titleContent}</h1>
+    <p>${descContent}</p>`;
+    titleContent = ""
+    descContent = "";
 })
 
 deleteBtn.addEventListener("click", (e)=>{
     e.preventDefault()
     localStorage.removeItem('todo')
+    todo.innerHTML=""
 });
